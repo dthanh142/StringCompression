4aa5aba26c3040facbd970434e7e2e5a41650708
teehee <dthanh142@gmail.com>
2021-03-04 21:46:05 +0700

    - Add logger

--- src/main/java/thanhpham/example/StringCompression/StringCompressServiceImpl.java
+++ src/main/java/thanhpham/example/StringCompression/StringCompressServiceImpl.java
@@ -1,30 +1,41 @@
 package thanhpham.example.StringCompression;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StringCompressServiceImpl implements StringCompressService {
 
+	final static Logger logger = Logger.getLogger(StringCompressionApplication.class);
+	
 	@Override
 	public String stringCompress(String msg) {
 		String result = "";
 		
-		for (int i = 0; i < msg.length(); i++) { 
+				
+		try { 
+			for (int i = 0; i < msg.length(); i++) { 
 
-			int count = 1; 
-			while (i < msg.length() - 1 && msg.charAt(i) == msg.charAt(i + 1)) { 
-				count++; 
-				i++; 
+				int count = 1; 
+				while (i < msg.length() - 1 && msg.charAt(i) == msg.charAt(i + 1)) { 
+					count++; 
+					i++; 
+				} 
+
+				if (count == 1) {
+					result += msg.charAt(i); 
+				} 
+				else {
+		    	  	result += msg.charAt(i); 
+		    	  	result += count; 
+				}
 			} 
 
-			if (count == 1) {
-				result += msg.charAt(i); 
-			} 
-			else {
-	    	  	result += msg.charAt(i); 
-	    	  	result += count; 
-			}
-		} 
+			
+		} catch(Exception ex) {
+			logger.error(ex);
+		}
+		
 		return result;
 	}
 
--- src/main/java/thanhpham/example/StringCompression/StringCompressionApplication.java
+++ src/main/java/thanhpham/example/StringCompression/StringCompressionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StringCompressionApplication {
-
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(StringCompressionApplication.class, args);
 	}
