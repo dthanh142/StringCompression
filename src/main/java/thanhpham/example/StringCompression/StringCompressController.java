55101b83d07e27d9c12190f1f5d40cd2155a4af1
teehee <dthanh142@gmail.com>
2021-03-04 23:44:45 +0700

    Add k8s deployment

--- src/main/java/thanhpham/example/StringCompression/StringCompressController.java
+++ src/main/java/thanhpham/example/StringCompression/StringCompressController.java
@@ -2,10 +2,12 @@ package thanhpham.example.StringCompression;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,6 +19,11 @@ public class StringCompressController {
 	@Autowired
 	StringCompressService stringCompressService;
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<String> root() {
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+	
 	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public String stringCompress(@RequestParam String message) {
 		return this.stringCompressService.stringCompress(message);
